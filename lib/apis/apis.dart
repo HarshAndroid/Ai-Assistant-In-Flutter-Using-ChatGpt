7a16912a6f8a76db0eced5835430d9801535d6ae
IdeHarsh <ideharsh2023@gmail.com>
2024-07-17 12:20:40 +0530

    *Added Google AI Gemini Integration.

--- lib/apis/apis.dart
+++ lib/apis/apis.dart
@@ -1,45 +1,74 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
 
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:http/http.dart';
 import 'package:translator_plus/translator_plus.dart';
 
 import '../helper/global.dart';
 
 class APIs {
-  //get answer from chat gpt
+  //get answer from google gemini ai
   static Future<String> getAnswer(String question) async {
     try {
-      //
-      final res =
-          await post(Uri.parse('https://api.openai.com/v1/chat/completions'),
+      log('api key: $apiKey');
 
-              //headers
-              headers: {
-                HttpHeaders.contentTypeHeader: 'application/json',
-                HttpHeaders.authorizationHeader: 'Bearer $apiKey'
-              },
+      final model = GenerativeModel(
+        model: 'gemini-1.5-flash-latest',
+        apiKey: apiKey,
+      );
 
-              //body
-              body: jsonEncode({
-                "model": "gpt-3.5-turbo",
-                "max_tokens": 2000,
-                "temperature": 0,
-                "messages": [
-                  {"role": "user", "content": question},
-                ]
-              }));
+      final content = [Content.text(question)];
+      final res = await model.generateContent(content, safetySettings: [
+        SafetySetting(HarmCategory.dangerousContent, HarmBlockThreshold.none),
+        SafetySetting(HarmCategory.sexuallyExplicit, HarmBlockThreshold.none),
+        SafetySetting(HarmCategory.harassment, HarmBlockThreshold.none),
+        SafetySetting(HarmCategory.hateSpeech, HarmBlockThreshold.none),
+      ]);
 
-      final data = jsonDecode(res.body);
+      log('res: ${res.text}');
 
-      log('res: $data');
-      return data['choices'][0]['message']['content'];
+      return res.text!;
     } catch (e) {
-      log('getAnswerE: $e');
+      log('getAnswerGeminiE: $e');
       return 'Something went wrong (Try again in sometime)';
     }
   }
+
+  //get answer from chat gpt
+  // static Future<String> getAnswer(String question) async {
+  //   try {
+  //     log('api key: $apiKey');
+
+  //     //
+  //     final res =
+  //         await post(Uri.parse('https://api.openai.com/v1/chat/completions'),
+
+  //             //headers
+  //             headers: {
+  //               HttpHeaders.contentTypeHeader: 'application/json',
+  //               HttpHeaders.authorizationHeader: 'Bearer $apiKey'
+  //             },
+
+  //             //body
+  //             body: jsonEncode({
+  //               "model": "gpt-3.5-turbo",
+  //               "max_tokens": 2000,
+  //               "temperature": 0,
+  //               "messages": [
+  //                 {"role": "user", "content": question},
+  //               ]
+  //             }));
+
+  //     final data = jsonDecode(res.body);
+
+  //     log('res: $data');
+  //     return data['choices'][0]['message']['content'];
+  //   } catch (e) {
+  //     log('getAnswerGptE: $e');
+  //     return 'Something went wrong (Try again in sometime)';
+  //   }
+  // }
 
   static Future<List<String>> searchAiImages(String prompt) async {
     try {
--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -6,6 +6,9 @@ const appName = 'Ai Assistant';
 //media query to store size of device screen
 late Size mq;
 
-//chat gpt api key (Updated it) - TODO
+// TODO Chat Gpt Api key OR
+//  Google Gemini API Key - https://aistudio.google.com/app/apikey
+//  You need to Update it in your Appwrite Project or comment appwrite code and hardcord key here.
+
 // String apiKey = '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';
 String apiKey = '';
